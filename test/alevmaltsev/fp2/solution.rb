ce727e031062491fa42af3afd0ddedf8d30adb0d
alevmaltsev <a.maltsev@restream.rt.ru>
2019-01-28 22:31:24 +0400

    do arrays and fp

---
+++ test/alevmaltsev/fp2/solution.rb
@@ -0,0 +1,25 @@
+module Alevmaltsev
+  module Fp2
+    class MyArray < Array
+      # Использовать стандартные функции массива для решения задач нельзя.
+      # Использовать свои написанные функции для реализации следующих - можно.
+
+      # Написать свою функцию my_each
+      def my_each(array)
+        for i in 0..array.length - 1
+      end
+
+      # Написать свою функцию my_map
+      def my_map
+      end
+
+      # Написать свою функцию my_compact
+      def my_compact
+      end
+
+      # Написать свою функцию my_reduce
+      def my_reduce
+      end
+    end
+  end
+end
