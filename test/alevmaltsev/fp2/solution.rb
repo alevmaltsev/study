18e739b25bbd4e6afd9cec814787b332338bae9e
alevmaltsev <a.maltsev@restream.rt.ru>
2019-01-29 08:26:11 +0400

    do fp2

--- test/alevmaltsev/fp2/solution.rb
+++ test/alevmaltsev/fp2/solution.rb
@@ -5,20 +5,33 @@ module Alevmaltsev
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each(array)
-        for i in 0..array.length - 1
+      def my_each
+        for el in self
+          yield el
+        end
+        self
       end
 
       # Написать свою функцию my_map
       def my_map
+        new_array = MyArray.new
+        my_each { |el| new_array.push yield el }
+        new_array
       end
 
       # Написать свою функцию my_compact
       def my_compact
+        new_array = MyArray.new
+        my_each { |el| new_array.push el if !el.nil? }
+        new_array
       end
 
       # Написать свою функцию my_reduce
-      def my_reduce
+      def my_reduce(memo = nil)
+        new_array = MyArray.new(self)
+        memo = memo.nil? ? new_array.shift : memo
+        new_array.my_each { |el| memo = yield el, memo }
+        memo
       end
     end
   end
